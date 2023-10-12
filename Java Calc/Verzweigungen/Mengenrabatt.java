91c063a27e2fdc78276dbe4f9a59a3cc37a7eecc
Igor Maciaszczyk <135438945+Shinivoa@users.noreply.github.com>
2023-10-12 08:40:13 +0200

    If/Else

---
+++ Java Calc/Verzweigungen/Mengenrabatt.java
@@ -0,0 +1,31 @@
+package Verzweigungen;
+
+import Tools.IOTools;
+
+public class Mengenrabatt {
+    public static void main(String[] args) {
+        float price, amount, years_of_business, rabatt, endprice;
+        price = 13.99f;
+        rabatt = 0.0f;
+        amount = IOTools.readFloat(" = ");
+        years_of_business = IOTools.readFloat(" = ");
+
+        endprice = price * amount;
+        System.out.println(endprice + "$");
+
+        if (amount < 500.0f) {
+            rabatt = 2.00f;
+        } else if (amount < 1000.0f) {
+            rabatt = 5.00f;
+        } else if (amount > 1000.0f) {
+            rabatt = 10.00f;
+        }
+        if (years_of_business > 10) {
+            rabatt += 5.00f;
+        }
+        System.out.println(rabatt + "%");
+        endprice = price * amount * (1 - rabatt / 100);
+        System.out.println(endprice + "$");
+    }
+
+}
